8d9255b52d928e77367db7e93b365e2c94f21c45
xen0dermu3 <xenonicus@gmail.com>
2023-10-24 09:32:26 -0700

    Added docs

--- src/core/iqair/iqair.controller.ts
+++ src/core/iqair/iqair.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Get, Query, NotFoundException } from '@nestjs/common';
-import { Root } from './interfaces';
+import { Controller, Get, NotFoundException, Query } from '@nestjs/common';
 import { IqairService } from './iqair.service';
+import { ApiQuery, ApiTags } from '@nestjs/swagger';
 
 @Controller('iqair')
+@ApiTags('iqair')
 export class IqairController {
   constructor(private readonly iqairService: IqairService) {
   }
 
   @Get('nearest-city-air-quality')
+  @ApiQuery({ name: 'longitude', required: false, type: String, example: '2.352222' })
+  @ApiQuery({ name: 'latitude', required: false, type: String, example: '48.856613' })
   async getAirQualityOfNearestCity(@Query('longitude') longitude: string, @Query('latitude') latitude: string) {
     const data = await this.iqairService.getAirQualityOfNearestCity(latitude, longitude);
 
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('app.port');
+
+  const config = new DocumentBuilder()
+    .setTitle('Air Quality')
+    .setDescription('API showing air quality')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(port);
 }
